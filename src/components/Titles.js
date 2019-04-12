755f7b8d66bf477766ac89562d5d11310206ab27
unknown <theitalianpeter@hotmail.com>
2019-04-12 18:58:13 +0100

    Css commit

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -13,8 +13,9 @@ import React from "react";
 
 const Titles = () => ( // in this case we don't need to put props, so we just put parentheses
         <div>
-            <h1>Weather Finder</h1>
-            <p>Find out temperature, conditions and more...</p>
+            <h1 className="title-container__title">Weather Finder</h1>
+            <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+                   
         </div>
      );
 
